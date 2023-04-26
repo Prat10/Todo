56a4125997475c6804d4c00372aef3b852acb53d
pramesh101 <prameshsingh2411@gmail.com>
2023-04-26 17:30:18 +0530

    Todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+export default function App() {
+  const [tasks, setTasks] = useState([]);
 
-function App() {
+  function onRemoveTask(taskToRemove) {
+    const newTasks = tasks.filter((task) => {
+      return task !== taskToRemove;
+    });
+    setTasks(newTasks);
+  }
+  //this function for submit the task
+  function onSubmit(event) {
+    event.preventDefault(); //it prevent default value
+    const form = event.target; //target the input 
+    const input = form.task;//target the value using name of task
+    const newTasks = [...tasks, input.value];// array which store all the values
+    setTasks(newTasks); // set the newTasks in the list
+    form.reset(); //we reset the input for next task
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className='App'>
+        <h1>MY Todo List</h1>
+        <h1>MY Todo List</h1>
+      </div>
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          name='task'
+          placeholder='Add the Tasks'
+          required
+        />
+        <button>Add</button>
+      </form>
+      <ul>
+        {tasks.map((task, index) => (
+          <Task onRemoveTask={onRemoveTask} key={task + index} task={task} />
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
+function Task({ task, onRemoveTask }) {
+  return (
+    <li>
+      {task}
+      <button className="delete" onClick={() => onRemoveTask(task)}>
+        x
+      </button>
+    </li>
+  );
+}
